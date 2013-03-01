339384098ebdc57615082c05a40ee32ec16ed452
Rahul <rahul@digitalgreen.org>
2013-03-01 13:13:32 -0500

    Changing the relationship betweeen journal and user

--- rpm/models.py
+++ rpm/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class SleepJournalEntry(models.Model):
-    user = models.OneToOneField(User)
+    user = models.ForeignKey(User)
     entry_date = models.DateField() 
     q1 = models.CharField(blank=True, max_length=5)
     q2 = models.CharField(blank=True, max_length=10)
